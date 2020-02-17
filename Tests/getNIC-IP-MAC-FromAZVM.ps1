631963d61470c5aed3c4f43e511ddfc4b28cabd4
Thomas Klawun <das70erjahr@hotmail.com>
2020-02-17 22:20:18 +0100

    Juhu, Die MAC habe ich.....

--- Tests/getNIC-IP-MAC-FromAZVM.ps1
+++ Tests/getNIC-IP-MAC-FromAZVM.ps1
@@ -1,6 +1,8 @@
 $vm = get-azvm -name 'testClWin10-02'
 $NI = ($vm).NetworkProfile.NetworkInterfaces
-(Get-AzNetworkInterface $NI).MacAddress
-
-#($NI) | Get-AzNetworkInterface
-
+#($NI).Id
+#Get-AzNetworkInterface 
+#(Get-AzNetworkInterface $NI).MacAddress
+#(Get-AzNetworkInterface $NI).VirtualMachine
+$MACADD = (($NI).id | Get-AzNetworkInterface).MacAddress
+$MACADD
