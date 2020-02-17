978ce53b355ed0fb1ba86a17a686cb27372084df
Thomas Klawun <das70erjahr@hotmail.com>
2020-02-17 22:23:24 +0100

    Clear Code

--- Tests/getNIC-IP-MAC-FromAZVM.ps1
+++ Tests/getNIC-IP-MAC-FromAZVM.ps1
@@ -1,8 +1,4 @@
 $vm = get-azvm -name 'testClWin10-02'
 $NI = ($vm).NetworkProfile.NetworkInterfaces
-#($NI).Id
-#Get-AzNetworkInterface 
-#(Get-AzNetworkInterface $NI).MacAddress
-#(Get-AzNetworkInterface $NI).VirtualMachine
 $MACADD = (($NI).id | Get-AzNetworkInterface).MacAddress
 $MACADD
