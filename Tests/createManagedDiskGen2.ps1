23f56bacf72e4d56f3db16ef73c1d97b55dbf741
Thomas Klawun <das70erjahr@hotmail.com>
2020-02-28 18:57:22 +0100

    lösche unnötige Zeilen

--- Tests/createManagedDiskGen2.ps1
+++ Tests/createManagedDiskGen2.ps1
@@ -7,7 +7,7 @@ $osDiskName = 'gen2Diskfrmgenvhd'  #<Provide a name for your disk>
 $diskconfig = New-AzDiskConfig -Location $location -DiskSizeGB 127 -AccountType Standard_LRS -OsType Windows -HyperVGeneration "V2" -SourceUri $osVhdUri -CreateOption 'Import' -StorageAccountId $storageAccountID 
 $azdiskGen2 = New-AzDisk -DiskName $osDiskName -ResourceGroupName $rgName -Disk $diskconfig
 $azdiskGen2
-#$imageConfig = New-AzImageConfig -Location $location
-#$imageConfig = Set-AzImageOsDisk -Image $imageConfig -OsType Windows -OsState Generalized -BlobUri $osVhdUri
-##$imageConfig = Set-AzImageOsDisk -Image $imageConfig -OsType Windows -OsState Specialized -BlobUri $osVhdUri 
-#$image = New-AzImage -ImageName $imageName -ResourceGroupName $rgName -Image $imageConfig
+Reactivate-AzBatchTask
+
+
+
